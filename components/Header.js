7c8ede479d1bab157fa482d3a9c750137593d76c
JoelcettGit <152936917+JoelcettGit@users.noreply.github.com>
2025-10-14 18:33:35 -0300

    error deploy solucionado

--- components/Header.js
+++ components/Header.js
@@ -11,19 +11,20 @@ export default function Header() {
         <header className="main-header">
             <div className="logo">
                 <Link href="/">
-                    {/* 2. Reemplaza <img> por <Image> */}
+                    {/* 2. Reemplaza <img> por <Image> con ancho y alto */}
                     <Image 
                         src="/logo-vidaanimada.png" 
                         alt="Vida Animada Logo"
-                        width={150} // Define un ancho
-                        height={50} // Define un alto
+                        width={150}
+                        height={50}
                         style={{ objectFit: 'contain', cursor: 'pointer' }}
+                        priority // Carga esta imagen primero, ya que está en el encabezado
                     />
                 </Link>
             </div>
             <nav className="main-nav">
                 <ul>
-                    {/* 3. Asegúrate de que todos los enlaces usen <Link> */}
+                    {/* 3. Asegúrate de que todos los enlaces usen <Link> correctamente */}
                     <li><Link href="/">Inicio</Link></li>
                     <li><Link href="/categorias">Categorías</Link></li>
                     <li><Link href="/#nosotros">Nosotros</Link></li>

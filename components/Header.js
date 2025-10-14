5f4eff89a184450218b62d1ea2da8a2bbe161532
JoelcettGit <152936917+JoelcettGit@users.noreply.github.com>
2025-10-14 18:36:23 -0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 // components/Header.js
 import Link from 'next/link';
-import Image from 'next/image'; // 1. Importa el componente Image
+import Image from 'next/image';
 import { useCart } from '../context/CartContext';
 
 export default function Header() {
@@ -11,20 +11,18 @@ export default function Header() {
         <header className="main-header">
             <div className="logo">
                 <Link href="/">
-                    {/* 2. Reemplaza <img> por <Image> con ancho y alto */}
                     <Image 
                         src="/logo-vidaanimada.png" 
                         alt="Vida Animada Logo"
                         width={150}
                         height={50}
                         style={{ objectFit: 'contain', cursor: 'pointer' }}
-                        priority // Carga esta imagen primero, ya que está en el encabezado
+                        priority
                     />
                 </Link>
             </div>
             <nav className="main-nav">
                 <ul>
-                    {/* 3. Asegúrate de que todos los enlaces usen <Link> correctamente */}
                     <li><Link href="/">Inicio</Link></li>
                     <li><Link href="/categorias">Categorías</Link></li>
                     <li><Link href="/#nosotros">Nosotros</Link></li>

541d2412081661754f74314fb894cd6e0aac68ba
JoelcettGit <152936917+JoelcettGit@users.noreply.github.com>
2025-10-14 18:49:48 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export default function HomePage({ products }) {
     <>
       <Head>
         <title>Vida Animada</title>
+        <link rel="icon" href="/logo-vidaanimada.png"/>
       </Head>
       <Header />
       <main>
